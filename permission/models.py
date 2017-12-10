be70ac143110199370b6d52e7c0dc64d9c47d215
jimmy201602 <zhengge2012@gmail.com>
2017-12-10 21:28:08 +0800

    add permission groups model

--- permission/models.py
+++ permission/models.py
@@ -14,4 +14,11 @@ class Role(models.Model):
         return self.name
     
 class RolePermission(models.Model):
-    pass
+    name = models.ForeignKey(Role)
+    users = models.ManyToManyField()
+    permission = models.ManyToManyField()
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
+    
+    def __unicode__(self):
+        return self.name.name    
