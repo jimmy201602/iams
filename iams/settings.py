d5841e2e929cb8a91b6a32dff8ee1da13b5dd337
jimmy201602 <zhengge2012@gmail.com>
2017-12-10 21:23:18 +0800

    add permission app

--- iams/settings.py
+++ iams/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'datatableview',
     'guardian',
     'cmdb',
+    'permission'
 ]
 
 MIDDLEWARE = [
---
+++ permission/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50,verbose_name="Name",unique=True,blank=False)
+    description = models.TextField(verbose_name="Description",blank=True)
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now_add=True,editable=False)
+    
+    def __unicode__(self):
+        return self.name
+    
+class RolePermission(models.Model):
+    pass
