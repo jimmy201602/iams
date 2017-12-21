5be88aad4d6271649957fe27747efb299370b6fb
jimmy201602 <zhengge2012@gmail.com>
2017-12-21 22:28:55 +0800

    add permission model

--- permission/models.py
+++ permission/models.py
@@ -2,40 +2,51 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-#class Role(models.Model):
-    #name = models.CharField(max_length=50,verbose_name="Name",unique=True,blank=False)
-    #description = models.TextField(verbose_name="Description",blank=True)
-    #createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
-    #updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
+class Permission(models.Model):
+    name = models.CharField(max_length=50,verbose_name="Name",unique=True,blank=False)
+    app = models.CharField(max_length=50,verbose_name="App",unique=True,blank=False)
+    model = models.CharField(max_length=50,verbose_name="Model",unique=True,blank=False)
+    description = models.TextField(verbose_name="Description",blank=True)
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
     
-    #def __unicode__(self):
-        #return self.name
+    def __unicode__(self):
+        return self.name
     
-#class RolePermission(models.Model):
-    #name = models.ForeignKey(Role)
-    #permission = models.ManyToManyField()
-    #createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
-    #updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
+class Role(models.Model):
+    name = models.CharField(max_length=50,verbose_name="Name",unique=True,blank=False)
+    description = models.TextField(verbose_name="Description",blank=True)
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
     
-    #def __unicode__(self):
-        #return self.name.name
+    def __unicode__(self):
+        return self.name
+    
+class RolePermission(models.Model):
+    name = models.ForeignKey(Role)
+    permission = models.ManyToManyField(Permission)
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
+    
+    def __unicode__(self):
+        return self.name.name
 
-#class RoleMember(models.Model):
-    #name = models.ForeignKey(Role)
-    #member = models.ManyToManyField()
-    #createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
-    #updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
+class RoleMember(models.Model):
+    name = models.ForeignKey(Role)
+    member = models.ManyToManyField(Permission)
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
     
-    #def __unicode__(self):
-        #return self.name.name
+    def __unicode__(self):
+        return self.name.name
 
-#class PermissionList(models.Model):
-    #name = models.CharField(max_length=50,verbose_name="App Name",unique=True,blank=False)
-    #permissions = models.ManyToManyField()
-    #createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
-    #updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
+class PermissionList(models.Model):
+    name = models.CharField(max_length=50,verbose_name="App Name",unique=True,blank=False)
+    permissions = models.ManyToManyField(Permission)
+    createtime = models.DateTimeField(verbose_name='Create time',auto_now_add=True,editable=False)
+    updatetime = models.DateTimeField(verbose_name='Update time',auto_now=True,editable=True)
     
-    #def __unicode__(self):
-        #return self.name    
+    def __unicode__(self):
+        return self.name    
