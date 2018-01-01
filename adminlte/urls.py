a2f2e122b87191c2b8b5577a2e99f83aa62fb89d
jimmy201602 <zhengge2012@gmail.com>
2018-01-01 11:09:07 +0800

    fix adminlte app index page bug

--- adminlte/urls.py
+++ adminlte/urls.py
@@ -3,7 +3,7 @@ from adminlte.views import Index,DynamicFormCreate,DynamicFormUpdate,DynamicMode
 
 urlpatterns = [
     url(r'^$', Index.as_view(),name='adminlte-index'),
-    url(r'^(?P<app_label>\w+)/(?P<model>\w+)/add/$', DynamicFormCreate.as_view(),name='dynamic-form-create'),
-    url(r'^(?P<app_label>\w+)/(?P<model>\w+)/(?P<pk>[0-9]+)/update/$', DynamicFormUpdate.as_view(),name='dynamic-form-update'),
-    url(r'^(?P<app_label>\w+)/(?P<model>\w+)/list/$', DynamicModelList.as_view(),name='dynamic-list'),
+    url(r'^/form/(?P<app_label>\w+)/(?P<model>\w+)/add/$', DynamicFormCreate.as_view(),name='dynamic-form-create'),
+    url(r'^/form/(?P<app_label>\w+)/(?P<model>\w+)/(?P<pk>[0-9]+)/update/$', DynamicFormUpdate.as_view(),name='dynamic-form-update'),
+    url(r'^/form/(?P<app_label>\w+)/(?P<model>\w+)/list/$', DynamicModelList.as_view(),name='dynamic-list'),
 ]
